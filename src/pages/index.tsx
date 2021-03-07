d72c01dfeb49467c3ab2bb714f3767340ddcbd4c
Bruno N. Campos <bruno@mambowifi.com>
2021-03-07 16:44:53 -0300

    First husky

---
+++ src/pages/index.tsx
@@ -0,0 +1,35 @@
+import Head from 'next/head'
+
+type Props = {
+  title: string
+}
+
+const foo = 'foo'
+
+export default function Home({ title = 'Next.js na Prática!' }: Props) {
+  return (
+    <div className="container">
+      <Head>
+        <title>{title}</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className="main">
+        <h1 className="title">
+          Welcome to <a href="https://nextjs.org">{title}</a>
+        </h1>
+      </main>
+
+      <footer className="footer">
+        <a
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Powered by{' '}
+          <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
+        </a>
+      </footer>
+    </div>
+  )
+}
